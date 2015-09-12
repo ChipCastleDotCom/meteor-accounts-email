4cdf085709d06efae7b2915b897b24ddbb91a248
Chip Castle <chip@chipcastle.com>
2015-09-11 23:02:02 -0500

    Update version after README update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chipcastledotcom:accounts-email',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Extract the email address from an Account object',
   // URL to the Git repository containing the source code for this package.
